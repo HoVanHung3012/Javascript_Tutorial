4ee97a7106317f7b044c48adb386c8be9e6d2c9f
Hồ Văn Hưng <hung2110054@gmail.com>
2024-05-30 18:25:40 +0700

    Add files via upload

---
+++ lesson_1/main.js
@@ -0,0 +1,14 @@
+// khai báo biến full name
+var fullName = 'Hồ Văn Hưng';
+var age = 26
+// Gọi hàm thông báo
+alert(fullName)
+alert(age)
+/* Một số hàm built-in
+     1. Alert
+     2. Console (log, warm, error)
+     3. confirm
+     4. prompt
+     5. set timeout
+     6. set interval
+*/
